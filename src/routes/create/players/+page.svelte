d078e9915c106e30077bc6d15478eb504884f856
Andukov Almaz <68683713+andiazdi@users.noreply.github.com>
2024-07-22 00:43:48 +0300

    Table component

--- src/routes/create/players/+page.svelte
+++ src/routes/create/players/+page.svelte
@@ -6,6 +6,13 @@
   import NavButtons from '../(components)/NavButtons.svelte';
   import { customGoto } from '$lib/utils';
   import { Plus, Upload } from 'lucide-svelte';
+  import Table from '$lib/components/Table.svelte';
+
+  let rankingsColumns = ['Name', 'Rating'];
+  let rankingsRows = [
+    ['Name Surname', '184'],
+    ['Eman Emanrus', '123'],
+  ];
 </script>
 
 <Navbar
@@ -21,20 +28,25 @@
 
 <Page>
   <div class="h-screen w-full bg-white p-4 gap-5 pt-52 px-8">
-    <div class="flex flex-col">
-      <div class="font-bold text-[22px] text-primary pb-5">Add player</div>
-      <Input variant="shadowed" class="mt-1 mb-5 w-[300px]" placeholder="Player's name" />
-      <Input variant="shadowed" class="mt-1 w-[300px] mb-8" placeholder="Rating (optional)" />
-      <Button class="w-[140px] mb-9">
-        Add
-        <Plus class="w-6 h-6" />
-      </Button>
-      <div class="flex flex-col items-center gap-2 md:pl-[6rem] md:items-start">
-        <div class="text-xl">or upload csv</div>
-        <Button variant="secondary">
-          Upload
-          <Upload class="w-6 h-6" />
+    <div class="flex flex-col md:flex-row gap-10">
+      <div class="flex flex-col">
+        <div class="font-bold text-[22px] text-primary pb-5">Add player</div>
+        <Input variant="shadowed" class="mt-1 mb-5 w-[300px]" placeholder="Player's name" />
+        <Input variant="shadowed" class="mt-1 w-[300px] mb-8" placeholder="Rating (optional)" />
+        <Button class="w-[140px] mb-9">
+          Add
+          <Plus class="w-6 h-6" />
         </Button>
+        <div class="flex flex-col items-center gap-2 md:pl-[6rem] md:items-start">
+          <div class="text-xl">or upload csv</div>
+          <Button variant="secondary">
+            Upload
+            <Upload class="w-6 h-6" />
+          </Button>
+        </div>
+      </div>
+      <div class="ml-auto">
+        <Table columns={rankingsColumns} rows={rankingsRows} />
       </div>
     </div>
   </div>
