8077ac657eae4c82b32e58a23a358805bc75eff7
Daniil Gazizullin <login@hikariatama.ru>
2024-07-22 11:55:16 +0000

    fix

--- src/routes/profile/active/+page.server.ts
+++ src/routes/profile/active/+page.server.ts
@@ -1,5 +1,6 @@
-import { getTournaments } from '$lib/server/db';
+import { deleteTournament, getTournaments } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export const load = (() => {
   const tournaments = getTournaments('active');
@@ -7,3 +8,15 @@ export const load = (() => {
     tournaments,
   };
 }) satisfies PageServerLoad;
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  default: async (event) => {
+    const formData = await event.request.formData();
+    if (formData.has('slug')) {
+      const slug = formData.get('slug') as string;
+      deleteTournament(slug);
+      throw redirect(303, '/profile/active');
+    }
+  },
+};
