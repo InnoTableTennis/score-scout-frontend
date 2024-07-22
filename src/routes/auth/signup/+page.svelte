e209ab22522199c931eee692ad3dd9e75324b314
morisummer <timofeevnikita111@gmail.com>
2024-07-22 14:44:20 +0300

    Fix eslint errors

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -25,7 +25,8 @@
       isLoading = false;
       if (event.result.status === 200) {
         toast.success("You've successfully signed up!");
-        document.cookie = `token=${event.result.data.token}; path=/`;
+        if ('data' in event.result && typeof event.result.data === 'object' && 'token' in event.result.data)
+          document.cookie = `token=${event.result.data.token}; path=/`;
         customGoto('/profile/active');
       } else {
         toast.error("Couldn't sign up, please try again.");
